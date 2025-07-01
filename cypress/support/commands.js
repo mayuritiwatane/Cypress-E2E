f59b8ee6bb28a5854e2946c6f41909d966f7354e
mayuritiwatane <mayuritiwatane9@gmail.com>
2025-07-01 18:46:16 +0530

    checkout test and file upload capability
    added new test and file upload utility

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,23 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import 'cypress-file-upload';
+Cypress.Commands.add('checkoutfunction',(CountryName)=>{
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    cy.xpath("//button[text()='ADD TO CART']").eq(4).click()
+    cy.xpath("//button[text()='ADD TO CART']").eq(5).click();
+    cy.xpath("//button[text()='ADD TO CART']").eq(10).click();
+    cy.get('img[alt="Cart"]').click();
+    cy.xpath("//button[text()='PROCEED TO CHECKOUT']").click();
+    cy.xpath("//button[text()='Place Order']").click();
+    cy.get('select').within(() => {
+  cy.get('option:disabled').invoke('removeAttr', 'disabled');
+});
+
+cy.get('select').select(CountryName); // Replace with actual visible text or value
+cy.get('[type="checkbox"]').check();
+cy.xpath("//button[text()='Proceed']").click();
+cy.get(".wrapperTwo").contains("Thank you");
+cy.url().should('include','/seleniumPractise/#/')
+
+})
